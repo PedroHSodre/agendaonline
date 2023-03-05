7082d6f5972ce49506c71ee0685898524567f5f4
Pedro Sodre <pedrosodredev@gmail.com>
2023-03-05 19:02:43 -0300

    add toastfy global

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { ToastContainer, toast } from 'react-toastify';
 
 import MainRoute from "./routes";
 
 import GlobalStyle from "./styles/global";
 import theme from "./styles/theme";
 
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <MainRoute />
+      <ToastContainer />
     </ThemeProvider>
   );
 }
