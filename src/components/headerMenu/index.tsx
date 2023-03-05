b19f82bd9559efe879bef52081a65ad8306c9bcc
Pedro Sodre <pedrosodredev@gmail.com>
2023-03-05 19:06:05 -0300

    navigate between screens

--- src/components/headerMenu/index.tsx
+++ src/components/headerMenu/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
 import { HeaderMenu, HeaderMenuItem } from "./styled";
 import { HeaderMenuProps } from "../../types/headerMenu";
+import { useNavigate } from "react-router-dom";
 
 export default function(props: HeaderMenuProps) {
     const { page } = props;
 
+    const navigate = useNavigate();
+
+    const handlePage = (page: string) => navigate(`/${page}`)
+
     return (
         <HeaderMenu>
-            <HeaderMenuItem isThisPage={page == 'Home'}>
+            <HeaderMenuItem isThisPage={page == 'Home'} onClick={() => handlePage('home')}>
                 Calendario
             </HeaderMenuItem>
-            <HeaderMenuItem isThisPage={page == 'manageSchedules'}>
+            <HeaderMenuItem isThisPage={page == 'manageSchedules'} onClick={() => handlePage('manage-schedules')}>
                 Gerenciar agendamentos
             </HeaderMenuItem>
             <HeaderMenuItem>
